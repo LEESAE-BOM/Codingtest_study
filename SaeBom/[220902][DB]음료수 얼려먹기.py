a091fb9944002f40c25c03994eedf4cced421da5
이새봄 <70834586+LEESAE-BOM@users.noreply.github.com>
2022-09-02 21:59:55 +0900

    Create [220902][DB]음료수 얼려먹기.py

---
+++ SaeBom/[220902][DB]음료수 얼려먹기.py
@@ -0,0 +1,22 @@
+N, M = map(int,input().split())
+graph=[]
+def DFS(graph,i,j):
+    if i<0 or i>=N or j<0 or j>=M:
+        return False
+    if graph[i][j]==0:
+        graph[i][j]=1
+        DFS(graph,i-1,j)
+        DFS(graph, i , j-1)
+        DFS(graph, i +1, j)
+        DFS(graph, i , j+1)
+        return True
+    return False
+
+for i in range(N+1):
+    graph.append(list(map(int,input())))
+count = 0
+for i in range(N):
+    for j in range(M):
+        if DFS(graph,i,j)==True:
+            count+=1
+print(count)
